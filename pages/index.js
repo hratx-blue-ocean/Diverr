21c55e2a6ff5a07cdbad0c86cd8e548922b85f37
Kenny Tran <kenny.tran138@gmail.com>
2021-05-21 18:01:32 -0500

    Implemented example button with material-ui

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,33 @@
 import Head from "next/head";
 import Image from "next/image";
 import AddButton from "common/components/buttons/AddButton";
+import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/styles";
+
+const useStyles = makeStyles({
+  root: {
+    height: "100vh",
+    width: "100vw",
+    overflow: "hidden",
+  },
+});
 
 export default function Home() {
+  const classes = useStyles();
   return (
     <div>
       <Head>
         <title>Caspian</title>
         <meta name="home" content="caspian-holder" />
       </Head>
-      <AddButton text={"Log Dive"} />
+      <Grid
+        className={classes.root}
+        container
+        justify="center"
+        alignItems="center"
+      >
+        <AddButton text={"Log Dive"} />
+      </Grid>
     </div>
   );
 }
