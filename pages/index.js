8107a471b34f9d201cea8d2f902c1cb9cd7b9aa8
Kenny Tran <kenny.tran138@gmail.com>
2021-05-23 21:20:02 -0500

    Implemented authorization flow for Google oAuth

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,9 @@ import Image from "next/image";
 import AddButton from "common/components/buttons/AddButton";
 import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/styles";
+import Header from "common/widgets/Header";
+import { useSession } from "next-auth/client";
+import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles({
   root: {
@@ -14,19 +17,27 @@ const useStyles = makeStyles({
 
 export default function Home() {
   const classes = useStyles();
+  const [session, loading] = useSession();
   return (
     <div>
       <Head>
         <title>Caspian</title>
         <meta name="home" content="caspian-holder" />
       </Head>
+      <Header />
       <Grid
         className={classes.root}
         container
+        direction="column"
         justify="center"
         alignItems="center"
       >
         <AddButton text={"Log Dive"} />
+        <Typography variant="h2" color="primary">
+          {loading && "Loading..."}
+          {session && `Hello, ${session.user.email}`}
+          {!session && !loading && "Please sign in"}
+        </Typography>
       </Grid>
     </div>
   );
