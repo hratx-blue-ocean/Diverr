d0cb54830fd8176d425e581552decdec7d529d57
Kenny Tran <kenny.tran138@gmail.com>
2021-05-24 16:36:05 -0500

    Changed import in Form to correctly catch Header which has moved

--- pages/form.js
+++ pages/form.js
@@ -1,25 +1,34 @@
 import React from "react";
 import ThemeWrapper from "styles/Theme";
 import { makeStyles } from "@material-ui/core/styles";
-import Header from "common/components/Header";
+import Header from "common/widgets/Header";
 import Grid from "@material-ui/core/Grid";
 import { useSession } from "next-auth/client";
-import { Typography, TextField, Checkbox, FormControlLabel, FormGroup, FormControl, FormLabel, Radio, RadioGroup } from '@material-ui/core';
+import {
+  Typography,
+  TextField,
+  Checkbox,
+  FormControlLabel,
+  FormGroup,
+  FormControl,
+  FormLabel,
+  Radio,
+  RadioGroup,
+} from "@material-ui/core";
 import AddButton from "common/components//buttons/AddButton.js";
-
 
 const useStyles = makeStyles((theme) => ({
   col: {
     display: "inline-flex",
-    flexDirection: "column"
+    flexDirection: "column",
   },
   checkboxes: {
     display: "inline-flex",
-    flexDirection: "row"
+    flexDirection: "row",
   },
   textfield: {
-    margin: 5
-  }
+    margin: 5,
+  },
 }));
 
 export default function AddNewLogForm() {
@@ -30,7 +39,6 @@ export default function AddNewLogForm() {
       <Header />
       <form>
         <Grid container spacing={3} direction="row">
-
           {/* Column 1 */}
 
           <Grid item xs={3}>
@@ -192,77 +200,81 @@ export default function AddNewLogForm() {
           <Grid item xs={3}>
             <div className={classes.checkboxes}>
               <Grid item xs={6}>
-                <Typography variant="h6">
-                  Exposure Protection
-                </Typography>
+                <Typography variant="h6">Exposure Protection</Typography>
                 <FormControl>
                   <FormLabel component="legend">Thermal Insulation</FormLabel>
                   <RadioGroup>
-                    <FormControlLabel value="Wet Suit" control={<Radio />} label="Wet Suit" />
-                    <FormControlLabel value="Dry Suit" control={<Radio />} label="Dry Suit" />
+                    <FormControlLabel
+                      value="Wet Suit"
+                      control={<Radio />}
+                      label="Wet Suit"
+                    />
+                    <FormControlLabel
+                      value="Dry Suit"
+                      control={<Radio />}
+                      label="Dry Suit"
+                    />
                   </RadioGroup>
                 </FormControl>
                 <FormGroup>
                   <FormControlLabel
-                    control={
-                      <Checkbox
-                        checked={false}
-                        name="Hood"
-                      />}
+                    control={<Checkbox checked={false} name="Hood" />}
                     label="Hood"
                   />
                   <FormControlLabel
-                    control={
-                      <Checkbox
-                        checked={false}
-                        name="Gloves"
-                      />}
+                    control={<Checkbox checked={false} name="Gloves" />}
                     label="Gloves"
                   />
                   <FormControlLabel
-                    control={
-                      <Checkbox
-                        checked={false}
-                        name="Boots"
-                      />}
+                    control={<Checkbox checked={false} name="Boots" />}
                     label="Boots"
                   />
                 </FormGroup>
               </Grid>
               <Grid item xs={6}>
-                <Typography variant="h6">
-                  Conditions
-                </Typography>
+                <Typography variant="h6">Conditions</Typography>
                 <FormControl>
                   <FormLabel component="legend">Environment</FormLabel>
                   <RadioGroup>
-                    <FormControlLabel value="Boat" control={<Radio />} label="Boat" />
-                    <FormControlLabel value="Shore" control={<Radio />} label="Shore" />
-                    <FormControlLabel value="Controlled" control={<Radio />} label="Controlled Environment" />
+                    <FormControlLabel
+                      value="Boat"
+                      control={<Radio />}
+                      label="Boat"
+                    />
+                    <FormControlLabel
+                      value="Shore"
+                      control={<Radio />}
+                      label="Shore"
+                    />
+                    <FormControlLabel
+                      value="Controlled"
+                      control={<Radio />}
+                      label="Controlled Environment"
+                    />
                   </RadioGroup>
                 </FormControl>
                 <FormControl>
                   <FormLabel component="legend">Water</FormLabel>
                   <RadioGroup>
-                    <FormControlLabel value="Fresh" control={<Radio />} label="Fresh" />
-                    <FormControlLabel value="Salt" control={<Radio />} label="Salt" />
+                    <FormControlLabel
+                      value="Fresh"
+                      control={<Radio />}
+                      label="Fresh"
+                    />
+                    <FormControlLabel
+                      value="Salt"
+                      control={<Radio />}
+                      label="Salt"
+                    />
                   </RadioGroup>
                 </FormControl>
                 <FormGroup>
                   <FormControlLabel
-                    control={
-                      <Checkbox
-                        checked={false}
-                        name="Waves"
-                      />}
+                    control={<Checkbox checked={false} name="Waves" />}
                     label="Waves"
                   />
                   <FormControlLabel
-                    control={
-                      <Checkbox
-                        checked={false}
-                        name="Current"
-                      />}
+                    control={<Checkbox checked={false} name="Current" />}
                     label="Current"
                   />
                 </FormGroup>
@@ -272,10 +284,10 @@ export default function AddNewLogForm() {
         </Grid>
         <div className={classes.col}>
           <h1>Placeholder for tags</h1>
-          <AddButton text={"Add Photos or Videos"}/>
-          <AddButton text={"Submit New Log"}/>
+          <AddButton text={"Add Photos or Videos"} />
+          <AddButton text={"Submit New Log"} />
         </div>
       </form>
     </>
-  )
+  );
 }
