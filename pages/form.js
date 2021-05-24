42d63fef0dbb0e7bc478efeb7149ceed6745c7f2
Amanda Davitt <acdavitt@gmail.com>
2021-05-24 14:47:57 -0500

    Created 4 columns (checkboxes in a separate column)

--- pages/form.js
+++ pages/form.js
@@ -4,111 +4,171 @@ import { makeStyles } from "@material-ui/core/styles";
 import Header from "common/components/Header";
 import Grid from "@material-ui/core/Grid";
 import { useSession } from "next-auth/client";
-import { Typography, TextField, Checkbox, FormControlLabel, FormGroup, FormControl, FormLabel, Radio, RadioGroup} from '@material-ui/core';
+import { Typography, TextField, Checkbox, FormControlLabel, FormGroup, FormControl, FormLabel, Radio, RadioGroup } from '@material-ui/core';
+
+
+const useStyles = makeStyles((theme) => ({
+  col: {
+    display: "inline-flex",
+    flexDirection: "column"
+  },
+  checkboxes: {
+    display: "inline-flex",
+    flexDirection: "row"
+  }
+}));
 
 export default function AddNewLogForm() {
+  const classes = useStyles();
+
   return (
     <>
       <Header />
       <form>
         <Grid container spacing={3} direction="row">
+
+          {/* Column 1 */}
+
           <Grid item xs={3}>
-            <Grid item>
+            <div className={classes.col}>
               <TextField
                 required
                 id="outlined-required"
                 label="Date"
                 variant="outlined"
               />
-            </Grid>
-            <Grid item>
               <TextField
                 required
                 id="outlined-required"
                 label="City"
                 variant="outlined"
               />
-            </Grid>
-            <Grid item>
               <TextField
                 required
                 id="outlined-required"
                 label="Country"
                 variant="outlined"
               />
-            </Grid>
-            <Grid item>
               <TextField
                 required
                 id="outlined-required"
                 label="Dive Site"
                 variant="outlined"
               />
-            </Grid>
-            <Grid item>
               <TextField
                 required
                 id="outlined-required"
                 label="Dive Center"
                 variant="outlined"
               />
-            </Grid>
-            <Grid item>
               <TextField
                 required
                 id="outlined-required"
                 label="Dive Instructor Name"
                 variant="outlined"
               />
-            </Grid>
-            <Grid item>
               <TextField
                 required
                 id="outlined-required"
                 label="Instructor Cert No."
                 variant="outlined"
               />
-            </Grid>
+            </div>
           </Grid>
+
+          {/* Column 2 */}
+
           <Grid item xs={3}>
-            <Grid item>
+            <div className={classes.col}>
+              <TextField
+                required
+                id="outlined-required"
+                label="Time In"
+                variant="outlined"
+              />
+              <TextField
+                required
+                id="outlined-required"
+                label="Time Out"
+                variant="outlined"
+              />
+              <TextField
+                required
+                id="outlined-required"
+                label="Start air pressure"
+                variant="outlined"
+              />
+              <TextField
+                required
+                id="outlined-required"
+                label="End air pressure"
+                variant="outlined"
+              />
+              <TextField
+                required
+                id="outlined-required"
+                label="Maximum Depth"
+                variant="outlined"
+              />
+              <TextField
+                required
+                id="outlined-number"
+                label="Actual Bottom Time"
+                variant="outlined"
+              />
+              <TextField
+                id="outlined-number"
+                label="Residual Nitrogen Time"
+                variant="outlined"
+              />
+              <TextField
+                id="outlined-number"
+                label="Total Bottom Time"
+                variant="outlined"
+              />
+            </div>
+          </Grid>
+
+          {/* Column 3 */}
+
+          <Grid item xs={3}>
+            <div className={classes.col}>
               <TextField
                 id="outlined"
                 label="Visibility"
                 variant="outlined"
               />
-            </Grid>
-            <Grid item>
               <TextField
                 id="outlined"
                 label="Air Temperature"
                 variant="outlined"
               />
-            </Grid>
-            <Grid item>
               <TextField
                 id="outlined-required"
                 label="Water Temperature"
                 variant="outlined"
               />
-            </Grid>
-            <Grid item>
               <TextField
                 id="outlined"
                 label="Weight"
                 variant="outlined"
               />
-            </Grid>
-            <Grid item xs={3} spacing={3}>
-              <Grid item xs={6} spacing={3}>
+            </div>
+          </Grid>
+
+          {/* Column 4 (radio and checkboxes) */}
+
+          <Grid item xs={3}>
+            <div className={classes.checkboxes}>
+              <Grid item xs={6}>
                 <Typography variant="h6">
                   Exposure Protection
                 </Typography>
                 <FormControl>
                   <FormLabel component="legend">Thermal Insulation</FormLabel>
                   <RadioGroup>
-                    <FormControlLabel value="Wet Suit" control={<Radio />} label="Wet Suit"/>
-                    <FormControlLabel value="Dry Suit" control={<Radio />} label="Dry Suit"/>
+                    <FormControlLabel value="Wet Suit" control={<Radio />} label="Wet Suit" />
+                    <FormControlLabel value="Dry Suit" control={<Radio />} label="Dry Suit" />
                   </RadioGroup>
                 </FormControl>
                 <FormGroup>
@@ -119,42 +179,42 @@ export default function AddNewLogForm() {
                         name="Hood"
                       />}
                     label="Hood"
-                    />
+                  />
                   <FormControlLabel
-                  control={
-                    <Checkbox
-                      checked={false}
-                      name="Gloves"
-                    />}
+                    control={
+                      <Checkbox
+                        checked={false}
+                        name="Gloves"
+                      />}
                     label="Gloves"
                   />
                   <FormControlLabel
-                  control={
-                    <Checkbox
-                      checked={false}
-                      name="Boots"
-                    />}
+                    control={
+                      <Checkbox
+                        checked={false}
+                        name="Boots"
+                      />}
                     label="Boots"
                   />
                 </FormGroup>
               </Grid>
-              <Grid item xs={6} spacing={3}>
+              <Grid item xs={6}>
                 <Typography variant="h6">
                   Conditions
                 </Typography>
                 <FormControl>
                   <FormLabel component="legend">Environment</FormLabel>
                   <RadioGroup>
-                    <FormControlLabel value="Boat" control={<Radio />} label="Boat"/>
-                    <FormControlLabel value="Shore" control={<Radio />} label="Shore"/>
-                    <FormControlLabel value="Controlled" control={<Radio />} label="Controlled Environment"/>
+                    <FormControlLabel value="Boat" control={<Radio />} label="Boat" />
+                    <FormControlLabel value="Shore" control={<Radio />} label="Shore" />
+                    <FormControlLabel value="Controlled" control={<Radio />} label="Controlled Environment" />
                   </RadioGroup>
                 </FormControl>
                 <FormControl>
                   <FormLabel component="legend">Water</FormLabel>
                   <RadioGroup>
-                    <FormControlLabel value="Fresh" control={<Radio />} label="Fresh"/>
-                    <FormControlLabel value="Salt" control={<Radio />} label="Salt"/>
+                    <FormControlLabel value="Fresh" control={<Radio />} label="Fresh" />
+                    <FormControlLabel value="Salt" control={<Radio />} label="Salt" />
                   </RadioGroup>
                 </FormControl>
                 <FormGroup>
@@ -165,82 +225,18 @@ export default function AddNewLogForm() {
                         name="Waves"
                       />}
                     label="Waves"
-                    />
+                  />
                   <FormControlLabel
-                  control={
-                    <Checkbox
-                      checked={false}
-                      name="Current"
-                    />}
+                    control={
+                      <Checkbox
+                        checked={false}
+                        name="Current"
+                      />}
                     label="Current"
                   />
                 </FormGroup>
               </Grid>
-            </Grid>
-          </Grid>
-          <Grid item xs={3} spacing={3}>
-            <Grid item>
-              <TextField
-                required
-                id="outlined-required"
-                label="Time In"
-                variant="outlined"
-              />
-            </Grid>
-            <Grid item>
-              <TextField
-                required
-                id="outlined-required"
-                label="Time Out"
-                variant="outlined"
-              />
-            </Grid>
-            <Grid item>
-              <TextField
-                required
-                id="outlined-required"
-                label="Start air pressure"
-                variant="outlined"
-              />
-            </Grid>
-            <Grid item>
-              <TextField
-                required
-                id="outlined-required"
-                label="End air pressure"
-                variant="outlined"
-              />
-            </Grid>
-            <Grid item>
-              <TextField
-                required
-                id="outlined-required"
-                label="Maximum Depth"
-                variant="outlined"
-              />
-            </Grid>
-            <Grid item>
-              <TextField
-                required
-                id="outlined-number"
-                label="Actual Bottom Time"
-                variant="outlined"
-              />
-            </Grid>
-            <Grid item>
-              <TextField
-                id="outlined-number"
-                label="Residual Nitrogen Time"
-                variant="outlined"
-              />
-            </Grid>
-            <Grid item>
-              <TextField
-                id="outlined-number"
-                label="Total Bottom Time"
-                variant="outlined"
-              />
-            </Grid>
+            </div>
           </Grid>
         </Grid>
       </form>
