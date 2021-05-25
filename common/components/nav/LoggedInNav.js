790f57871cd108135e127c3d52f0c073b4809059
Kenny Tran <kenny.tran138@gmail.com>
2021-05-25 14:07:24 -0500

    Replaced nested Links with Typography

--- common/components/nav/LoggedInNav.js
+++ common/components/nav/LoggedInNav.js
@@ -1,11 +1,11 @@
 import { signOut } from "next-auth/client";
 import Breadcrumbs from "@material-ui/core/Breadcrumbs";
-import Link from "@material-ui/core/Link";
 import Button from "@material-ui/core/Button";
 import ExitToAppOutlinedIcon from "@material-ui/icons/ExitToAppOutlined";
 import DescriptionOutlinedIcon from "@material-ui/icons/DescriptionOutlined";
 import OpacityOutlinedIcon from "@material-ui/icons/OpacityOutlined";
 import AddSharpIcon from "@material-ui/icons/AddSharp";
+import Typography from "@material-ui/core/Typography";
 
 export default function LoggedInNav({ classes }) {
   return (
@@ -20,36 +20,33 @@ export default function LoggedInNav({ classes }) {
           <OpacityOutlinedIcon className={classes.icon} href="/feed" />
         }
       >
-        <Link className={classes.menuItem} data-testid="feed" href="/feed">
+        <Typography className={classes.menuItem} data-testid="feed">
           Feed
-        </Link>
+        </Typography>
       </Button>
       <Button
         startIcon={<DescriptionOutlinedIcon className={classes.icon} />}
         href="/dashboard"
       >
-        <Link className={classes.menuItem} data-testid="logs" href="/dashboard">
+        <Typography className={classes.menuItem} data-testid="logs">
           View Logs
-        </Link>
+        </Typography>
       </Button>
       <Button
-        startIcon={<AddSharpIcon className={classes.icon} href="/form" />}
+        startIcon={<AddSharpIcon className={classes.icon} />}
+        href="/form"
       >
-        <Link className={classes.menuItem} data-testid="form" href="/form">
+        <Typography className={classes.menuItem} data-testid="form">
           Create Log
-        </Link>
+        </Typography>
       </Button>
       <Button
         startIcon={<ExitToAppOutlinedIcon className={classes.icon} />}
         onClick={signOut}
       >
-        <Link
-          onClick={signOut}
-          className={classes.menuItem}
-          data-testid="signOut"
-        >
+        <Typography className={classes.menuItem} data-testid="signOut">
           SIGN OUT
-        </Link>
+        </Typography>
       </Button>
     </Breadcrumbs>
   );
