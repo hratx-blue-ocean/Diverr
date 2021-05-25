539e85d578167d6a57ce803e4c5636012743599d
Jeff Jiang <jjiang4566@gmail.com>
2021-05-25 16:28:35 -0500

    remove yarn

--- pages/feed.js
+++ pages/feed.js
@@ -2,6 +2,7 @@ import Header from "common/widgets/Header";
 import Grid from "@material-ui/core/Grid";
 
 import Post from "common/widgets/Post.js";
+//import SearchBar from "common/widgets/SearchBar.js";
 
 export default function Feed() {
   return (
