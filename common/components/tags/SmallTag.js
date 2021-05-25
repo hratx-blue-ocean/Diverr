8721f5205a1e2283e612670c9cf437cf9d7217b0
Jeff Jiang <jjiang4566@gmail.com>
2021-05-25 09:25:22 -0500

    create small tag components and small tags container

---
+++ common/components/tags/SmallTag.js
@@ -0,0 +1,15 @@
+import Chip from "@material-ui/core/Chip";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  chip: {
+    margin: theme.spacing(0.5),
+  },
+}));
+
+export default function SmallTag({ tagName }) {
+  const classes = useStyles();
+  return (
+    <Chip label={tagName} size="small" color="primary" className={classes.chip} />
+  );
+}
---
+++ pages/feed.js
@@ -0,0 +1,18 @@
+import Header from "common/components/Header";
+import Grid from "@material-ui/core/Grid";
+
+export default function Feed() {
+  return (
+    <>
+      <Header />
+      <Grid
+        container
+        direction="column"
+        justify="flex-start"
+        alignItems="center"
+      >
+
+      </Grid>
+    </>
+  );
+}
