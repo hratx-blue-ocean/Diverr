cfc3c0a9b4da3d7bd3b18280a16a2523d1d3fca8
Jeff Jiang <jjiang4566@gmail.com>
2021-05-26 01:39:51 -0500

    delete console.log

--- pages/feed.js
+++ pages/feed.js
@@ -1,12 +1,11 @@
-import Header from "common/widgets/Header";
 import Grid from "@material-ui/core/Grid";
 
+import Header from "common/widgets/Header";
 import Post from "common/widgets/Post.js";
 // import SearchBar from "common/widgets/SearchBar.js";
 import data from "lib/dummyData/dummyData.js";
 
 export default function Feed() {
-  console.log(data.allLogs.logs);
   return (
     <>
       <Header />
