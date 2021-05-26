8f922f669789ad783823651e2e5f84daf07b6e5f
Jeff Jiang <jjiang4566@gmail.com>
2021-05-26 14:41:55 -0500

    next button bug fix

--- common/widgets/Posts/PostImages.js
+++ common/widgets/Posts/PostImages.js
@@ -62,14 +62,6 @@ export default function PostImages({ photos }) {
     }
   };
 
-  if (photos.length === 0) {
-    photos.push({
-      id: 1,
-      url: "https://images.unsplash.com/photo-1589634749362-a8ef3056cbe9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2775&q=80",
-      id_log: 1,
-    });
-  }
-
   return (
     <div className={classes.root}>
       <img
