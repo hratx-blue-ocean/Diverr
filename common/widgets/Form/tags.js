aca3e69b7327b4dd114f8304935fafb529086273
Amanda Davitt <acdavitt@gmail.com>
2021-05-26 16:34:20 -0500

    Able to add tags to new log

--- common/widgets/Form/tags.js
+++ common/widgets/Form/tags.js
@@ -1,12 +1,19 @@
 import { React, useState } from "react";
-import { Chip, TextField, Button} from "@material-ui/core";
+import { Chip, TextField, Button } from "@material-ui/core";
 import FormSmallTag from 'common/components/Form/chip.js';
+import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/styles";
 
+const useStyles = makeStyles((theme) => ({
+  tags: {
+    display: "inline-flex",
+    flexDirection: "column",
+    marginLeft: 50,
+  },
+}));
 
-
-export default function FormTags() {
-  const [tag, setTag] = useState();
-  const [tags, setTags] = useState([]);
+export default function FormTags({ tags, setTags }) {
+  const [tag, setTag] = useState('');
 
   const handleChange = (e) => {
     setTag(e.target.value);
@@ -20,18 +27,24 @@ export default function FormTags() {
   }
 
   return (
-    <>
-      <TextField
-        onChange={handleChange}
-        label="Add Tags"
-        name="tags"
-        variant="outlined"
-      />
-      <Button onClick={handleSubmit} color="primary" variant="contained" fullWidth>
-        Add a Tag
-      </Button>
-      {console.log({tags})}
-      {tags.map(chip => <FormSmallTag key={chip} chip={chip} tags={tags} setTags={setTags}/>)}
-    </>
+    <Grid container spacing={3} direction="row">
+      <Grid item xs={2}>
+        <TextField
+          onChange={handleChange}
+          label="Add Tags"
+          name="tags"
+          variant="outlined"
+          value={tag}
+        />
+      </Grid>
+      <Grid item xs={2}>
+        <Button onClick={handleSubmit} color="primary" variant="contained" fullWidth>
+          Add a Tag
+          </Button>
+      </Grid>
+      <Grid item xs={8}>
+        {tags.map(chip => <FormSmallTag key={chip} chip={chip} tags={tags} setTags={setTags} />)}
+      </Grid>
+    </Grid>
   )
 }
