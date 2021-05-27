ffb75237368a136e0677c11dd9d60527fa19ee11
Cameron Crow <19ccrow99@gmail.com>
2021-05-27 12:09:08 -0500

    Fixed conflicts

--- pages/api/user/[email]/tags.js
+++ pages/api/user/[email]/tags.js
@@ -1,6 +1,5 @@
 import executeUserTagsQuery from "lib/db/userTagsQuery";
 
-<<<<<<< HEAD
 export default function getUserTags(req, res) {
     executeUserTagsQuery(req.query.email, (err, response) => {
       if (err) {
@@ -9,14 +8,4 @@ export default function getUserTags(req, res) {
         res.send(response);
       }
     })
-=======
-export default function getUser(req, res) {
-  executeUserQuery(req.query.email, (err, response) => {
-    if (err) {
-      console.error(err);
-    } else {
-      res.send(response);
-    }
-  });
->>>>>>> master
 }
