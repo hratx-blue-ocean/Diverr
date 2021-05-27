a73815ea755f0c6e69cc4861b1d487465cc16d09
Jeff Jiang <jjiang4566@gmail.com>
2021-05-27 15:37:05 -0500

    delete get request for tag in home page

--- pages/index.js
+++ pages/index.js
@@ -126,10 +126,9 @@ export async function getServerSideProps(context) {
     res.end();
     return { props: {} };
   }
-  const resultTags = await axios.get(`${process.env.NEXTAUTH_URL}/api/tags`); // REPLACE EMAIL WITH ${email} IN THE FUTURE
-  const resultLogs =  await axios.get(`${process.env.NEXTAUTH_URL}/api/public`);// REPLACE EMAIL WITH ${email} IN THE FUTURE
+  const resultLogs =  await axios.get(`${process.env.NEXTAUTH_URL}/api/public`);
   const result = {
-    props: {resultTags: resultTags.data, resultLogs: resultLogs.data[0].logs}
+    props: {resultLogs: resultLogs.data[0].logs}
   }
   return result;
 }
