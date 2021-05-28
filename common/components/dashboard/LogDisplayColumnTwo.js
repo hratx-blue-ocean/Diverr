52080ae0cc2d907f215870ea56571074eb876cdf
Eren Serpin <erenserpin@gmail.com>
2021-05-27 20:40:08 -0400

    Finished making details gray

--- common/components/dashboard/LogDisplayColumnTwo.js
+++ common/components/dashboard/LogDisplayColumnTwo.js
@@ -25,7 +25,7 @@ export default function logDisplayColumn1({ log }) {
         <Typography variant="body1" className={classes.detail}> <Typography display="inline" color="primary" variant="body1">Visibility:</Typography>{' ' + log.visibility}</Typography>
         <Typography variant="body1" className={classes.detail}><Typography display="inline" color="primary" variant="body1">Air Temp:</Typography> {' ' + log.air_temp}</Typography>
         <Typography variant="body1" className={classes.detail}><Typography display="inline" color="primary" variant="body1">Water Temp: </Typography> {log.water_temp}</Typography>
-        <Typography variant="body1">
+        <Typography variant="body1" className={classes.detail}>
         <Typography display="inline" color="primary" variant="body1">Conditions: </Typography> {log.salt_water ? "Salt-Water" : "Fresh-Water"},{" "}
           {log.boat ? "Boat" : "No Boat"},{" "}
           {log.current ? "Current" : "No Current"}
@@ -34,10 +34,10 @@ export default function logDisplayColumn1({ log }) {
       <Box className={classes.spacer} />
       <Card className={classes.item}>
         <Typography variant="h6" color="primary">Dive Gear</Typography>
-        <Typography variant="body1">
+        <Typography variant="body1" className={classes.detail}>
           {log.wetsuit ? "Wet Suit" : "Dry Suit"}
         </Typography>
-        <Typography variant="body1">
+        <Typography variant="body1" className={classes.detail}>
           {log.hood ? "Hood, " : "No Hood,"}
           {log.gloves ? "Gloves " : "No Gloves"}
         </Typography>
@@ -46,14 +46,14 @@ export default function logDisplayColumn1({ log }) {
       <Box className={classes.spacer} />
       <Card className={classes.item}>
         <Typography variant="h6" color="primary">Dive Metrics</Typography>
-        <Typography variant="body1">
+        <Typography variant="body1" className={classes.detail}>
         <Typography display="inline" color="primary" variant="body1">Start Pressure: </Typography> {log.start_pressure}
         </Typography>
-        <Typography variant="body1">
+        <Typography variant="body1" className={classes.detail}>
         <Typography display="inline" color="primary" variant="body1">End Pressure: </Typography> {log.end_pressure}
         </Typography>
-        <Typography variant="body1"><Typography display="inline" color="primary" variant="body1">Start Time: </Typography> {log.time_in}</Typography>
-        <Typography variant="body1"><Typography display="inline" color="primary" variant="body1">End Time: </Typography> {log.time_out}</Typography>
+        <Typography variant="body1" className={classes.detail}><Typography display="inline" color="primary" variant="body1">Start Time: </Typography> {log.time_in}</Typography>
+        <Typography variant="body1" className={classes.detail}><Typography display="inline" color="primary" variant="body1">End Time: </Typography> {log.time_out}</Typography>
       </Card>
     </>
   );
