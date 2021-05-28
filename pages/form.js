5c255589a355277eb37f4f84706033142d718c5c
Amanda Davitt <acdavitt@gmail.com>
2021-05-27 19:26:42 -0500

    Moved axios req to w/in getServerSideProps

--- pages/form.js
+++ pages/form.js
@@ -33,45 +33,32 @@ const useStyles = makeStyles((theme) => ({
 export async function getServerSideProps(context) {
   const { req, res } = context;
   const session = await getSession({ req });
-  console.log(session.user.email)
-
-  return {
-    props: {
-      email: session.user.email,
-    },
-  };
+  if (!session) {
+    res.writeHead(302, { Location: '/' })
+    res.end()
+    return {
+      props: {}
+    }
+  }
+  if (session) {
+    const timeAndId = await axios.get(`${process.env.NEXTAUTH_URL}/api/user/sunilrgadgil@gmail.com/cumulative`);
+    console.log({timeAndId})
+    return {
+      props: {
+        email: session.user.email,
+        // cumulative_time: Number(timeAndId.data.sum),
+        // userId: timeAndId.data.id
+      },
+    };
+  }
 }
 
 
-export default function AddNewLogForm({ email }) {
+export default function AddNewLogForm({ email, cumulative_time, userId }) {
+  console.log({cumulative_time})
   const classes = useStyles();
   const [tags, setTags] = useState([]);
   const [images, setImages] = useState([]);
-  const [userId, setUserId] = useState(0);
-  const [cumulativeTime, setCumulativeTime] = useState(0)
-  const [allTags, setAllTags] = useState([])
-
-  useEffect(() => {
-    // const getUserInfo = async (email) => {
-    //   const userInfo = await axios.get(`/api/user/${email}`);
-    //   setUserId(userInfo.data)
-    //   console.log('user:', userInfo.data)
-    // }
-
-    const getAllTags = async () => {
-      //${process.env.NEXTAUTH_URL}
-      const allTags = await axios.get(`http://localhost:3000/api/tags`);
-      setAllTags(allTags.data)
-    }
-
-    const getCumulativeTime = async (email) => {
-      const cumulative_time = await axios.get(`http://localhost:3000/api/user/sunilrgadgil@gmail.com/cumulative`);
-      setCumulativeTime(cumulative_time.data)
-    }
-    // getUserInfo(email)
-    getAllTags();
-    getCumulativeTime(email);
-  }, [])
 
   const formik = useFormik({
     initialValues: {
@@ -104,11 +91,13 @@ export default function AddNewLogForm({ email }) {
       if (values.privacy === 'public') {
         values.public = true;
       }
+      delete values.privacy;
 
       //thermal insulation
       if (values.suitUp === 'wetsuit') {
         values.wet_suit = true;
       }
+      delete values.suitUp;
 
       //environment
       if (values.environment === 'controlled') {
@@ -118,38 +107,42 @@ export default function AddNewLogForm({ email }) {
           values.boat = true;
         }
       }
+      delete values.environment;
 
       //fresh v salt
       if (values.water === 'salt') {
         values.salt_water = true;
       }
+      delete values.water;
 
       //checkboxes
-      for (let item of values.checked) {
-        values[item] = true;
+      if (values.checked) {
+        for (let item of values.checked) {
+          values[item] = true;
+        }
       }
 
       //times
       values.abt = Number(values.abt);
       values.rnt = Number(values.rnt);
       values.tbt = Number(values.tbt);
-      values.cumulative_time = cumulativeTime + values.abt
+      values.cumulative_time = cumulative_time + values.abt;
 
-      console.log({values})
+      console.log({ values })
       alert(JSON.stringify(values, null, 2));
       console.log(JSON.stringify(values, null, 2));
 
-      const tagsToBeAdded = [];
-      for (let tag of values.tags) {
-        if (!allTags.includes(tag)) {
-          tagsToBeAdded.push(tag)
-        }
-      }
+      // const tagsToBeAdded = [];
+      // for (let tag of values.tags) {
+      //   if (!allTags.includes(tag)) {
+      //     tagsToBeAdded.push(tag)
+      //   }
+      // }
 
       // const addNewTags = (tagsToBeAdded) => {
-        // axios.post(`http://localhost:3000/api/tags`, {tags: tagsToBeAdded})
-        // .then(result => console.log('Tags added!'))
-        // .catch(err => console.log('Error adding tags'))
+      // axios.post(`http://localhost:3000/api/tags`, {tags: tagsToBeAdded})
+      // .then(result => console.log('Tags added!'))
+      // .catch(err => console.log('Error adding tags'))
       // }
 
       // addNewTags(tagsToBeAdded);
@@ -174,7 +167,7 @@ export default function AddNewLogForm({ email }) {
             <Column4 formik={formik} />
           </Grid>
           <Grid className={classes.col} container spacing={3}>
-            <FormTags tags={tags} setTags={setTags}/>
+            <FormTags tags={tags} setTags={setTags} />
             <FormMedia images={images} setImages={setImages} />
           </Grid>
           <Grid justify="center" container spacing={3}>
